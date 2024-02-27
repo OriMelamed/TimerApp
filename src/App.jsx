daa6b9e12174ca8c71c74985ea861cf03ef732b1
OriMelamed <ori.melamed@gmail.com>
2024-02-27 21:40:12 +0200

    mia test1

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
     <>
       <Player />
       <div id="challenges">
-        <h1>Maia</h1>
         <TimerChallenge title="Easy" targetTime={1} />
         <TimerChallenge title="Not Easy" targetTime={5} />
         <TimerChallenge title="Getting tough" targetTime={10} />
